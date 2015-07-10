e888a95a65696038af696b0bc1f2354ddddcd0af
Tyler Ellis <tyler.ellis21@gmail.com>
2015-07-10 04:40:28 -0700

    Added the mutation rate to the genome breeder class.

--- src/simulation/genetics/breeder.cpp
+++ src/simulation/genetics/breeder.cpp
@@ -1,7 +1,8 @@
 #include "breeder.h"
 #include "../../util/mathutils.h"
-#include "../../util/log.h"
+#include "../../core/console.h"
 
+#include <sstream>
 #include <stdlib.h>
 
 Genome Breeder::replicate(const Genome& parent)
@@ -12,28 +13,47 @@ Genome Breeder::replicate(const Genome& parent)
 
     const real32* parentWeights = parent.readWeights();
 
-    int mutationCount = 0;
+    // Rescale the mutation rate back up since it is stored as a float value.
+    int32 mutationRate = Genome::unscaleMutationRate(parentWeights[CELL_MUTATION_RATE_INDEX]);
+
+    // Make sure we don't go under the minimum value.
+    if (mutationRate < 1) {
+        mutationRate = 1;
+    }
+
+    int32 mutationCount = 0;
+
+    // Copy from parent, fine tune from parent, and randomized
+    // You can thank waterlimon for the advice we got here.
+
+    // TODO: Improve this method??
 
     real32* weights = genome.editWeights();
     for (int32 i = 0; i < size; i++) {
 
-        int dice = rand() % 100;
 
-        // Most of the time we directly copy.
-        if (dice >= 25) {
+        int dice = rand() % mutationRate;
+        if (dice >= 50) {
+
+            // Most of the time we directly copy.
             weights[i] = parentWeights[i];
         }
-        // Copy with a mutation.
+        else if (dice <= 25) {
+
+            // Copy with a mutation.
+            weights[i] = parentWeights[i] + randomFloat(-0.02f, 0.02f);
+            mutationCount += 1;
+        }
         else {
-            weights[i] = parentWeights[i] + randomFloat();
+            // Generate a new random genome weight.
+            weights[i] = Genome::randomGenomeWeight();
             mutationCount += 1;
         }
     }
 
     std::stringstream sb;
-    sb << "Mutation count: " << mutationCount;
-
-    Log::info(sb.str());
+    sb << "mutation count: " << mutationCount;
+    Console::write(sb.str());
 
     return genome;
 }
