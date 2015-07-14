215cb1ad7bc4c2f43d647b69943b045b7f936da6
Tyler Ellis <tyler.ellis21@gmail.com>
2015-07-13 19:38:54 -0700

    Fixed an issue with the last merge. Removed the old source which was missed in the last merge.

--- src/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "core/client.h"
-
-int main(int argc, char* args[]) {
-
-    Client client;
-    return client.start();
-}
--- src/util/timehelper.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <string>
-
-// Get current date/time, format is YYYY-MM-DD.HH:mm:ss.
-const std::string currentDateTime();
-
